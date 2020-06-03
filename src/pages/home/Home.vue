ed1000576f7e99473129328d805b8c6443db416b
easyfun2 <caixiaowei0001@126.com>
2020-06-03 23:13:08 +0800

    components header.vue

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- 这边为什么是home-header?? -->
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from '../home/components/Header'
+
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+
+}
+</script>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="herder-input">输入城市/景点/游玩主题</div>
+    <div class="herder-right">城市</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeHeader'
+
+}
+</script>
+
+// 1 rem = html font-size = 50px
+<style lang="stylus" scoped>
+  .header
+    display flex
+    height: .86rem
+    background #00bcd4
+    color #fff
+    line-height .86rem
+    .herder-left
+      width: .64rem
+      float left
+    .herder-input
+      flex 1
+      // 用到lineheight的化，要定死
+      height 64rem
+      line-height .64rem
+      height .64rem
+      margin-top .12rem
+      margin-left .2rem
+      background-color #fff
+      border-radius .1rem
+      color #ccc
+    .herder-right
+      width 1.24rem
+      float right
+
+</style>
